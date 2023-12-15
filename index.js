814548a173a05bdaa08706c0f0b1d9fead8703b0
MICHELLE NASIRISI <wanjikunasirisi1@gmail.com>
2023-12-16 02:55:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+// Function to return the first two drivers in the array
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // Function to return the last two drivers in the array
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // Array containing the two functions defined above
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // Higher-order function that returns a function to multiply a fare by a given factor
+  const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  };
+  
+  // Function to double a fare using createFareMultiplier
+  const fareDoubler = createFareMultiplier(2);
+  
+  // Function to triple a fare using createFareMultiplier
+  const fareTripler = createFareMultiplier(3);
+  
+  // Function to select different drivers based on the provided function
+  const selectDifferentDrivers = function(drivers, selectFunction) {
+    return selectFunction(drivers);
+  };
+  
+
+  module.exports = {
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+    selectingDrivers,
+    createFareMultiplier,
+    fareDoubler,
+    fareTripler,
+    selectDifferentDrivers,
+  };
+  
